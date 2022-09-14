105125c43a12f1af5980bae0551c524b40c416b4
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2022-09-14 16:05:00 +0500

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Dev Studio ⚡️ 
-
-## A clean, beautiful and responsive portfolio template for Developers!
-
-Just Change the portfolio.js file to get up and running
-
-WIP To be added
---
+++ precache-manifest.0ca4ac796b138895cf8b5ad07b5b805f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9186f76c7ac2ac5a39a24baec0a5002e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "111122c5d5ea146e9930",
+    "url": "/static/css/3.6a352120.chunk.css"
+  },
+  {
+    "revision": "8e526d3a08fdc7766c9f",
+    "url": "/static/css/4.6e14a8dd.chunk.css"
+  },
+  {
+    "revision": "7a029ce94cf56e5ae189",
+    "url": "/static/css/main.a6b2506d.chunk.css"
+  },
+  {
+    "revision": "685ba5397f9ff3032391",
+    "url": "/static/js/2.f8717900.chunk.js"
+  },
+  {
+    "revision": "3dbd45c8ed21d092cdcb3d5851d22288",
+    "url": "/static/js/2.f8717900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111122c5d5ea146e9930",
+    "url": "/static/js/3.eb91a7e6.chunk.js"
+  },
+  {
+    "revision": "8e526d3a08fdc7766c9f",
+    "url": "/static/js/4.640dd7ab.chunk.js"
+  },
+  {
+    "revision": "7a029ce94cf56e5ae189",
+    "url": "/static/js/main.fed58622.chunk.js"
+  },
+  {
+    "revision": "cafb1add77a0430235e8",
+    "url": "/static/js/runtime-main.46d66db1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "69feb01fbcc4fcdda65890a8ce987c97",
+    "url": "/static/media/FastLogo.69feb01f.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "32f64607d32feaf0f0069dfcf436774f",
+    "url": "/static/media/arbisoftLogo.32f64607.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "5391161a6e24e166a8ef5fa77af66d52",
+    "url": "/static/media/litterati.5391161a.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "22bb9b38b5ea1267c8a37471b54154ae",
+    "url": "/static/media/mprep.22bb9b38.jpeg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
